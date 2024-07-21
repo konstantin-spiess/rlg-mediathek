e48b6613df699cb94306f2a2afbc8227db6391fc
Konstantin Spieß <mail@konsti.co>
2024-07-21 17:33:35 +0200

    feat: Add dark mode support to RLG logo in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center  justify-center gap-4 p-24">
         <p className="text-center">Wilkommen in der RLG Mediathek.</p>
         <div className="flex flex-col gap-4">
-          <Image src="/rlg_logo.webp" alt="RLG Logo" width={200} height={200} />
+          <Image src="/rlg_logo.webp" alt="RLG Logo" width={200} height={200} className="dark:invert" />
         </div>
       </main>
     </div>
