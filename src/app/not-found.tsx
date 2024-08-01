bb462c0fb8c7618fff0b54ac8ec905861bcbd84e
Konstantin Spieß <mail@konsti.co>
2024-08-01 02:37:19 +0200

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center p-24">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-lg">Diese Seite konnte nicht gefunden werden.</p>
+    </div>
+  );
+}
