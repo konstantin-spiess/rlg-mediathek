f2ad8cfe64d24953faa5e05a613b2d5415332ef0
Konstantin Spieß <mail@konsti.co>
2024-08-01 02:25:42 +0200

    set runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 
+export const runtime = 'edge';
+
 const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
