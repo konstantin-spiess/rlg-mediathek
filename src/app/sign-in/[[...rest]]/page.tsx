a37ffdb92f92580a8e02b6bd69c260b836d2522f
Konstantin Spieß <mail@konsti.co>
2024-08-01 03:28:19 +0200

    Update sign-in and sign-up page layout for mobile responsiveness

--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center pt-24">
+    <div className="flex justify-center pt-24 max-sm:p-5 ">
       <SignIn />
     </div>
   );
--- src/app/sign-up/[[...rest]]/page.tsx
+++ src/app/sign-up/[[...rest]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
   return (
-    <div className="flex justify-center pt-24">
+    <div className="flex justify-center pt-24 max-sm:p-5">
       <SignUp />
     </div>
   );
